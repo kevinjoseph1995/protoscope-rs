09da882640015b14a9c2ae93df765708a0789c30
Kevin Joseph <kevinjoseph1995@gmail.com>
2023-09-23 08:33:58 -0700

    More lexer cleanup before moving on

--- rs-protoc/src/lexer.rs
+++ rs-protoc/src/lexer.rs
@@ -218,7 +218,6 @@ struct Lexer<'storage> {
     current_line_column: usize,
     current_line_number: usize,
     current_line_start_char_offset: usize,
-    number_of_characters_consumed: usize,
     seen_error: bool,
 }
 
@@ -229,7 +228,6 @@ impl<'storage> Lexer<'storage> {
             cursor: Cursor::new(source_text),
             current_line_column: 0,
             current_line_number: 0,
-            number_of_characters_consumed: 0,
             current_line_start_char_offset: 0,
             seen_error: false,
         }
@@ -241,7 +239,7 @@ impl<'storage> Lexer<'storage> {
         loop {
             if let Some(ch) = self.cursor.peek() {
                 if ch.is_alphanumeric() || ch == '_' {
-                    _ = self.next_char_with_index();
+                    _ = self.next_char();
                 } else {
                     break;
                 }
@@ -269,7 +267,7 @@ impl<'storage> Lexer<'storage> {
         loop {
             if let Some(ch) = self.cursor.peek() {
                 if ch.is_ascii_digit() {
-                    _ = self.next_char_with_index();
+                    _ = self.next_char();
                 } else {
                     break;
                 }
@@ -282,7 +280,7 @@ impl<'storage> Lexer<'storage> {
         loop {
             if let Some(ch) = self.cursor.peek() {
                 if ch.is_ascii_hexdigit() {
-                    _ = self.next_char_with_index();
+                    _ = self.next_char();
                 } else {
                     break;
                 }
@@ -297,7 +295,7 @@ impl<'storage> Lexer<'storage> {
             if let Some(ch) = self.cursor.peek() {
                 match ch {
                     '0'..='7' => {
-                        _ = self.next_char_with_index();
+                        _ = self.next_char();
                     }
                     _ => {
                         break;
@@ -317,7 +315,7 @@ impl<'storage> Lexer<'storage> {
             if let Some(ch) = self.cursor.peek() {
                 if ch == 'X' || ch == 'x' {
                     radix = Radix::Hexadecimal;
-                    _ = self.next_char_with_index();
+                    _ = self.next_char();
                 }
             }
         }
@@ -379,7 +377,7 @@ impl<'storage> Lexer<'storage> {
                 if ch == '.' {
                     // Example case: "123.666" Fractional part = .666
                     start = self.cursor.get_current_index();
-                    _ = self.next_char_with_index();
+                    _ = self.next_char();
                     self.consume_decimal_digits();
                     end = self.cursor.get_current_index();
                 }
@@ -398,13 +396,13 @@ impl<'storage> Lexer<'storage> {
         if let Some(ch) = self.cursor.peek() {
             match ch {
                 'e' | 'E' => {
-                    _ = self.next_char_with_index();
+                    _ = self.next_char();
                     span.start += 1;
                     if let Some(ch) = self.cursor.peek() {
                         match ch {
                             '+' | '-' => {
                                 // Consume optional '+'/'-' after the 'e'/'E'
-                                _ = self.next_char_with_index();
+                                _ = self.next_char();
                             }
                             _ => {}
                         }
@@ -435,7 +433,7 @@ impl<'storage> Lexer<'storage> {
             Ok(integral_part) => integral_part,
             Err(err) => {
                 return Some(Token {
-                    kind: self.get_error_token(err.to_string().as_str(), None),
+                    kind: self.get_error_token(err.to_string().as_str()),
                     line_number: self.current_line_number,
                     column_index: self.current_line_column,
                 });
@@ -446,7 +444,7 @@ impl<'storage> Lexer<'storage> {
             Ok(exponent_part) => exponent_part,
             Err(err) => {
                 return Some(Token {
-                    kind: self.get_error_token(err.to_string().as_str(), None),
+                    kind: self.get_error_token(err.to_string().as_str()),
                     line_number: self.current_line_number,
                     column_index: self.current_line_column,
                 });
@@ -461,7 +459,7 @@ impl<'storage> Lexer<'storage> {
                     Ok(value) => value,
                     Err(err) => {
                         return Some(Token {
-                            kind: self.get_error_token(err.to_string().as_str(), None),
+                            kind: self.get_error_token(err.to_string().as_str()),
                             line_number: self.current_line_number,
                             column_index: self.current_line_column,
                         });
@@ -487,7 +485,7 @@ impl<'storage> Lexer<'storage> {
                     Ok(value) => value,
                     Err(err) => {
                         return Some(Token {
-                            kind: self.get_error_token(err.to_string().as_str(), None),
+                            kind: self.get_error_token(err.to_string().as_str()),
                             line_number: self.current_line_number,
                             column_index: self.current_line_column,
                         });
@@ -509,7 +507,7 @@ impl<'storage> Lexer<'storage> {
                 Ok(value) => value,
                 Err(err) => {
                     return Some(Token {
-                        kind: self.get_error_token(err.to_string().as_str(), None),
+                        kind: self.get_error_token(err.to_string().as_str()),
                         line_number: self.current_line_number,
                         column_index: self.current_line_column,
                     });
@@ -529,33 +527,21 @@ impl<'storage> Lexer<'storage> {
     fn string_literal(&mut self, string_literal_header: char) -> Option<Token<'storage>> {
         // We've already consumed the quote
         debug_assert!(string_literal_header == '\'' || string_literal_header == '\"');
-        let string_literal_start_index = self.number_of_characters_consumed;
+        let string_literal_start_index = self.cursor.get_current_index();
         let mut escaped_sequence = String::new();
         loop {
-            if let Some((index, ch)) = self.next_char_with_index() {
+            if let Some(ch) = self.next_char() {
                 match ch {
                     '\n' => {
                         return Some(Token {
-                            kind: self.get_error_token(
-                                "Unterminated string literal",
-                                Some(Span {
-                                    start: string_literal_start_index,
-                                    end: index,
-                                }),
-                            ),
+                            kind: self.get_error_token("Unterminated string literal"),
                             line_number: self.current_line_number,
                             column_index: self.current_line_column,
                         });
                     }
                     '\x00' => {
                         return Some(Token {
-                            kind: self.get_error_token(
-                                "Unterminated string literal",
-                                Some(Span {
-                                    start: string_literal_start_index,
-                                    end: index,
-                                }),
-                            ),
+                            kind: self.get_error_token("Unterminated string literal"),
                             line_number: self.current_line_number,
                             column_index: self.current_line_column,
                         });
@@ -564,18 +550,15 @@ impl<'storage> Lexer<'storage> {
                         // Start of escape sequence
                         if escaped_sequence.is_empty() {
                             // Trigger a dynamic allocation and capture all the characters until the start of the escape sequence
-                            escaped_sequence
-                                .push_str(&self.source_text[string_literal_start_index..index]);
+                            escaped_sequence.push_str(
+                                &self.source_text[string_literal_start_index
+                                    ..self.cursor.get_current_index() - 1],
+                            );
                         }
                         if !self.consume_escape_sequence(&mut escaped_sequence) {
                             return Some(Token {
-                                kind: self.get_error_token(
-                                    "Invalid escape sequence in string literal",
-                                    Some(Span {
-                                        start: string_literal_start_index,
-                                        end: index,
-                                    }),
-                                ),
+                                kind: self
+                                    .get_error_token("Invalid escape sequence in string literal"),
                                 line_number: self.current_line_number,
                                 column_index: self.current_line_column,
                             });
@@ -594,7 +577,8 @@ impl<'storage> Lexer<'storage> {
                         } else {
                             return Some(Token {
                                 kind: TokenKind::StringLiteral(YarnBox::new(
-                                    &self.source_text[string_literal_start_index..index],
+                                    &self.source_text[string_literal_start_index
+                                        ..self.cursor.get_current_index() - 1],
                                 )),
                                 line_number: self.current_line_number,
                                 column_index: self.current_line_column,
@@ -610,13 +594,7 @@ impl<'storage> Lexer<'storage> {
                 }
             } else {
                 return Some(Token {
-                    kind: self.get_error_token(
-                        "Unterminated string literal",
-                        Some(Span {
-                            start: string_literal_start_index,
-                            end: self.source_text.len(),
-                        }),
-                    ),
+                    kind: self.get_error_token("Unterminated string literal"),
                     line_number: self.current_line_number,
                     column_index: self.current_line_column,
                 });
@@ -626,7 +604,7 @@ impl<'storage> Lexer<'storage> {
 
     fn next_token(&mut self) -> Option<Token<'storage>> {
         self.consume_whitespace_and_comments();
-        while let Some((index, ch)) = self.next_char_with_index() {
+        while let Some(ch) = self.next_char() {
             match ch {
                 ';' => {
                     return Some(Token {
@@ -750,13 +728,7 @@ impl<'storage> Lexer<'storage> {
                 'a'..='z' | 'A'..='Z' | '_' => return self.identifier_or_keyword(ch),
                 _ => {
                     return Some(Token {
-                        kind: self.get_error_token(
-                            "Unknown character",
-                            Some(Span {
-                                start: index,
-                                end: index + 1,
-                            }),
-                        ),
+                        kind: self.get_error_token("Unknown character"),
                         line_number: self.current_line_number,
                         column_index: self.current_line_column,
                     })
@@ -766,7 +738,7 @@ impl<'storage> Lexer<'storage> {
         None
     }
 
-    fn get_error_token(&mut self, message: &str, _span: Option<Span>) -> TokenKind<'storage> {
+    fn get_error_token(&mut self, message: &str) -> TokenKind<'storage> {
         self.seen_error = true;
         let mut error_message = format!("Lexer error {}\n", message);
         error_message += format!("{}", self.current_line_number).as_str();
@@ -811,7 +783,7 @@ impl<'storage> Lexer<'storage> {
             }
             if let Some(ch) = self.cursor.peek() {
                 if is_whitespace(ch) {
-                    _ = self.next_char_with_index(); // Consume the whitespace and move ahead
+                    _ = self.next_char(); // Consume the whitespace and move ahead
                     continue;
                 }
             }
@@ -820,10 +792,9 @@ impl<'storage> Lexer<'storage> {
         }
     }
 
-    fn next_char_with_index(&mut self) -> Option<(usize, char)> {
+    fn next_char(&mut self) -> Option<char> {
         match self.cursor.next_with_index() {
             Some((index, ch)) => {
-                self.number_of_characters_consumed += 1;
                 if ch == '\n' {
                     self.current_line_number += 1;
                     self.current_line_column = 1;
@@ -833,7 +804,7 @@ impl<'storage> Lexer<'storage> {
                 } else {
                     self.current_line_column += 1;
                 }
-                return Some((index, ch));
+                return Some(ch);
             }
             None => return None,
         }
@@ -843,11 +814,11 @@ impl<'storage> Lexer<'storage> {
     fn consume_single_line_comment(&mut self) {
         debug_assert!(self.cursor.peek().is_some());
         debug_assert!(self.cursor.peek().unwrap() == '/');
-        _ = self.next_char_with_index(); // Consume the "/"
+        _ = self.next_char(); // Consume the "/"
         debug_assert!(self.cursor.peek().is_some());
         debug_assert!(self.cursor.peek().unwrap() == '/');
-        _ = self.next_char_with_index(); // Consume the "/"
-        while let Some((_, ch)) = self.next_char_with_index() {
+        _ = self.next_char(); // Consume the "/"
+        while let Some(ch) = self.next_char() {
             if ch == '\n' || ch == '\x00' {
                 break;
             }
@@ -857,13 +828,13 @@ impl<'storage> Lexer<'storage> {
     fn consume_block_comment(&mut self) {
         debug_assert!(self.cursor.peek().is_some());
         debug_assert!(self.cursor.peek().unwrap() == '/');
-        _ = self.next_char_with_index(); // Consume the "/"
+        _ = self.next_char(); // Consume the "/"
         debug_assert!(self.cursor.peek().is_some());
         debug_assert!(self.cursor.peek().unwrap() == '*');
-        _ = self.next_char_with_index(); // Consume the "*"
-        while let Some((_, ch)) = self.next_char_with_index() {
+        _ = self.next_char(); // Consume the "*"
+        while let Some(ch) = self.next_char() {
             if ch == '*' {
-                if let Some((_, next_ch)) = self.next_char_with_index() {
+                if let Some(next_ch) = self.next_char() {
                     if next_ch == '/' {
                         break;
                     }
@@ -874,13 +845,13 @@ impl<'storage> Lexer<'storage> {
 
     fn consume_hex_escape_sequence(&mut self, escaped_string: &mut String) -> bool {
         let mut decoded_char: u32;
-        if let Some((_, first_required_char)) = self.next_char_with_index() {
+        if let Some(first_required_char) = self.next_char() {
             if first_required_char.is_ascii_hexdigit() {
                 let digit = first_required_char.to_digit(16).unwrap(); // SAFETY: We  just checked above that the character is a valid hex digit.
                 decoded_char = digit;
                 if let Some(second_optional_character) = self.cursor.peek() {
                     if second_optional_character.is_ascii_hexdigit() {
-                        _ = self.next_char_with_index(); // Consume the second hex digit
+                        _ = self.next_char(); // Consume the second hex digit
                         let lower_nibble = second_optional_character.to_digit(16).unwrap(); // SAFETY: We  just checked above that the character is a valid hex digit.
                         let upper_nibble = decoded_char << 4;
                         decoded_char = upper_nibble | lower_nibble;
@@ -909,7 +880,7 @@ impl<'storage> Lexer<'storage> {
                 if is_octal_digit(optional_digit) {
                     let optional_digit = optional_digit.to_digit(8).unwrap();
                     decoded_byte = decoded_byte << 3 | optional_digit;
-                    _ = self.next_char_with_index(); // Consume the digit
+                    _ = self.next_char(); // Consume the digit
                 }
             }
         }
@@ -925,8 +896,8 @@ impl<'storage> Lexer<'storage> {
         let mut consume_n_hex_digits = |n: usize| -> bool {
             let mut decoded_value: u32 = 0;
             for _ in 0..n {
-                match self.next_char_with_index() {
-                    Some((_, ch)) => {
+                match self.next_char() {
+                    Some(ch) => {
                         if ch.is_ascii_hexdigit() {
                             let nibble = ch.to_digit(16).unwrap();
                             decoded_value = (decoded_value << 4) | nibble; // SAFETY: We  just checked above that the character is a valid hex digit.
@@ -954,8 +925,8 @@ impl<'storage> Lexer<'storage> {
     }
 
     fn consume_escape_sequence(&mut self, escaped_string: &mut String) -> bool {
-        match self.next_char_with_index() {
-            Some((_, ch)) => match ch {
+        match self.next_char() {
+            Some(ch) => match ch {
                 'a' => {
                     escaped_string.push('\x07'); // Alert bell
                     return true;
